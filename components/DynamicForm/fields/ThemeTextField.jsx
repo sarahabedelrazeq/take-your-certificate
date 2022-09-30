d6941d580567befc44d723e64ff29a783f666b67
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2022-09-30 11:27:40 +0300

    done

--- components/DynamicForm/fields/ThemeTextField.jsx
+++ components/DynamicForm/fields/ThemeTextField.jsx
@@ -2,19 +2,16 @@ import { TextField } from "@mui/material";
 import React from "react";
 
 export default function ThemeTextField(props) {
-
   return (
     <TextField
       {...props}
       InputLabelProps={{
         ...props.InputLabelProps,
         style: { color: "white" },
-
       }}
       InputProps={{
         ...props.InputProps,
         style: { color: "white", borderColor: "white" },
-
       }}
       FormHelperTextProps={{
         ...props.FormHelperTextProps,
